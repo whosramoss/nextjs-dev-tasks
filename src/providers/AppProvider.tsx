ff8c69926d403a663a56d4fa3dbb7217d62599b1
Gabriel Ramos <whosramoss@gmail.com>
2025-01-31 10:57:37 -0300

    chore: create providers

---
+++ src/providers/AppProvider.tsx
@@ -0,0 +1,11 @@
+
+import { CustomQueryProvider } from "@providers/QueryProvider";
+import { ICommons } from "@utils/utils";
+
+export default async function AppProvider({ children }: ICommons) {
+  return (
+    <CustomQueryProvider>
+      {children}
+    </CustomQueryProvider>
+  );
+}
